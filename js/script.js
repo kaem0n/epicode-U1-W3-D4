9d87ea087df6dac298c2237e1b588473a6d84e94
kaem0n <antonio.ruggiapiquer@gmail.com>
2023-12-10 22:20:55 +0100

    project finished!

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const sortedNumber = document.getElementById('number')
 
 const numbers = []
 
-// GENERATORE DI CELLE
+// GENERATORE DI CELLE DEL TABELLONE + CESTO DI NUMERI
 
 const cellGen = function () {
     for (let i=0; i<90; i++) {
@@ -23,6 +23,7 @@ cellGen()
 // GESTORE PLAYERS
 
 let totalPTables = 0
+let totalPlayers = 0
 playerGen.addEventListener('submit', function(e) {
     e.preventDefault()
     let n = 0
@@ -39,25 +40,30 @@ playerGen.addEventListener('submit', function(e) {
         player.innerHTML = `<h3>${inputName.value}</h3>
         <div class="pTable"></div>
         `
+        totalPlayers += 1
         totalPTables += 1
         n += 1
+        playerContainer.appendChild(player)
     } else if (option2.selected) {
         player.innerHTML = `<h3>${inputName.value}</h3>
         <div class="pTable"></div>
         <div class="pTable"></div>
         `
+        totalPlayers += 1
         totalPTables += 2
         n += 2
+        playerContainer.appendChild(player)
     } else if (option3.selected) {
         player.innerHTML = `<h3>${inputName.value}</h3>
         <div class="pTable"></div>
         <div class="pTable"></div>
         <div class="pTable"></div>
         `
+        totalPlayers += 1
         totalPTables += 3
         n += 3
+        playerContainer.appendChild(player)
     }
-    playerContainer.appendChild(player)
     inputName.value = ''
     
     const pTables = document.getElementsByClassName('pTable')
@@ -68,7 +74,7 @@ playerGen.addEventListener('submit', function(e) {
         for (let j=0; j<24; j++) {
             let x = Math.floor(Math.random() * counter)
             const cell = document.createElement('div')
-            cell.classList.add('cell')
+            cell.classList.add('pCell')
             cell.innerHTML = `<span>${moreNumbers[x]}</span>`
             currentPTable.appendChild(cell)
             moreNumbers.splice(moreNumbers.indexOf(parseInt(cell.innerText)), 1)
@@ -81,13 +87,39 @@ playerGen.addEventListener('submit', function(e) {
 
 roulette.addEventListener('submit', function(e) {
     e.preventDefault()
-    const x = Math.floor(Math.random() * numbers.length)
-    const cells = document.getElementsByClassName('cell')
-    console.log(cells)
-    sortedNumber.innerText = numbers[x]
-    if (typeof numbers[x] === 'undefined') {
-        sortedNumber.innerText = 'Hai finito i numeri!'
+    if (totalPlayers === 0 || totalPlayers === 1) {
+        sortedNumber.innerText = 'Seleziona almeno due giocatori!'
+    } else {
+        playerGen.style.display = 'none'
+        const x = Math.floor(Math.random() * numbers.length)
+        const cells = document.getElementsByClassName('cell')
+        const playerCells = document.getElementsByClassName('pCell')
+        sortedNumber.innerText = numbers[x]
+        if (typeof numbers[x] === 'undefined') {
+            sortedNumber.innerText = 'Hai finito i numeri!'
+        } else {
+            cells[numbers[x]-1].classList.add('sorted')
+            for (let i=0; i<playerCells.length; i++) {
+                if (playerCells[i].innerText === sortedNumber.innerText) {
+                    playerCells[i].classList.add('sorted')
+                }
+            }
+            numbers.splice(numbers.indexOf(numbers[x]), 1)
+        }
     }
-    cells[numbers[x]-1].classList.add('sorted')
-    numbers.splice(numbers.indexOf(numbers[x]), 1)
 })
+
+// COPYRIGHT YEAR
+
+const currentYear = function () {
+    const now = new Date()
+    const currentYear = now.getFullYear()
+    const footerSpan = document.querySelector('footer span')
+    footerSpan.innerText = `©${currentYear} kaem0n`
+}
+
+currentYear()
+
+// const test = function () {
+
+// }
